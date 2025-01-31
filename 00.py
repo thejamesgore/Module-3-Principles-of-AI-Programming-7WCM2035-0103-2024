7417b57c5479fd485ecd1ce5eb9b02412ec8ba32
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-01-31 11:11:20 +0000

    Exploring the data types

---
+++ 00.py
@@ -0,0 +1,16 @@
+a = 4
+b = 5.3
+c = True
+d = "some text"
+e = {1,2,3,3,3,3,3,3}
+f = [1,2,3,4,5,6,7]
+g = (2,3,4,5,6,5,3)
+
+print("Print_variables function")
+print("a ia an", type(a), "and =", a)
+print("b ia an", type(b), "and =", b)
+print("c ia an", type(c), "and =", c)
+print("d ia an", type(d), ", has a length of", len(d), "and =", d)
+print("d ia an", type(e), ", has a length of", len(e), "and =", e)
+print("d ia an", type(f), ", has a length of", len(f), "and =", f)
+print("d ia an", type(g), ", has a length of", len(g), "and =", g)
