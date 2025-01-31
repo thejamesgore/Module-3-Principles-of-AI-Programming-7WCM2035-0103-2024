947059b8a1a3b7a00e82fe6721c0121ca6b03727
James Gore <83005220+thejamesgore@users.noreply.github.com>
2025-01-31 11:18:57 +0000

    Basic calculations

---
+++ 01.py
@@ -0,0 +1,11 @@
+a = 4
+b = 5.3
+
+def print_calculations():
+    print("print_calculations function")
+    print("a + b =", a + b)
+    print("a * b =", a * b)
+    print("a / b =", a / b)
+    print("a - b =", a - b)
+    
+print_calculations()
